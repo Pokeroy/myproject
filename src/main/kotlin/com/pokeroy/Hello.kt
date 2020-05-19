98fd4c93fd99b7e881c4521737aed8df7d48d6bf
Pokeroy <r971232@gmail.com>
2020-05-19 21:11:49 +0800

    data type

--- src/main/kotlin/com/pokeroy/Hello.kt
+++ src/main/kotlin/com/pokeroy/Hello.kt
@@ -4,6 +4,10 @@ fun main(args: Array<String>) {
 //    println("你好，Kotlin!!")
     val human = Human()
     human.sayHello()
+
+    val myName:String = "Pokeroy";
+    var number = 1;
+    println(myName+" "+number);
 }
 
 
